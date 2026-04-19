df152d133ff7d9d481f585e7eb081a19344a2dfe
Seshadri Raghav <165247517+Seshadriraghav22@users.noreply.github.com>
2026-04-19 18:26:03 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -10,5 +10,5 @@ terraform {
 provider "google" {
   region = "us-east-1"
   project = "genuine-eon-476919-k6"
-  credentials = file("C:/Users/ragha/Downloads/genuine-eon-476919-k6-3962b387b402.json")
+  
 }
